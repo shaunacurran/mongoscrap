2209c429650d204e5c60c9f2a0e70bb36f8a93a2
Seana Curran <shaunacurran@Shaunas-MacBook-Pro.local>
2017-09-24 20:14:19 -0500

    "updated scraping"

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
+//Dependencies
 
-
+var express = require("express");
+var mongojs = require("mongojs");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+// Requiring our Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+//requiring cheerio and request
 var cheerio = require("cheerio");
 var request = require("request");
+
+//initialize express
+var app = express();
+app.use(express.static("public"));
+
+//db config
+var dbName = "news-scraper";
+var collections = ["dataScraped"];
+
+//website scrapped
+var websiteURL = "https://www.thestreet.com";
+
+//link mongo to db
+var db = mongojs(dbName, collections);
+
+db.on("error", function (error) {
+  console.log("Error", error);
+});
 
 // Make a request call to grab the HTML body from the site of your choice
 request("https://www.thestreet.com/", function(error, response, html) {
@@ -31,3 +57,4 @@ request("https://www.thestreet.com/", function(error, response, html) {
   // Log the results once you've looped through each of the elements found with cheerio
   console.log(results);
 });
+
