97c85939ae783a924457935153cc83dc22d83449
Shauna Curran <shaunaccurran@gmail.com>
2017-09-24 23:22:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var Article = require("./models/Article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
-// Set mongoose to leverage built in JavaScript ES6 Promises
+
 mongoose.Promise = Promise;
 
 var PORT = process.env.PORT || 8080;
