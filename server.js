018562eb0ecf354243ec2e0aebedabe8e7746e35
Seana Curran <shaunacurran@Shaunas-MacBook-Pro.local>
2017-09-24 22:05:30 -0500

    "server with mongoose"

--- server.js
+++ server.js
@@ -1,81 +1,57 @@
-//Dependencies
-
+// Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
-// var bodyParser = require("body-parser");
-// var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 // Requiring our Note and Article models
-// var Note = require("./models/Note.js");
-// var Article = require("./models/Article.js");
-//requiring cheerio and request
-var cheerio = require("cheerio");
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+// Our scraping tools
 var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
 
-//initialize express
+var PORT = process.env.PORT || 8080;
+
+// Initialize Express
 var app = express();
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Serve static content
 app.use(express.static("public"));
 
-//db config
-var dbName = "news-scraper";
-var collections = ["articles"];
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-//website scrapped
-var websiteURL = "https://www.thestreet.com";
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-//link mongo to db
-var db = mongojs(dbName, collections);
+// Import routes and give the server access to them.
+var routes = require("./controllers/scraper.js");
 
-db.on("error", function (error) {
-  console.log("Error", error);
+app.use("/", routes);
+
+mongoose.connect("mongodb://localhost/news-scraper");
+
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Error: ", error);
 });
 
-app.get("/scrape", function (req, res) {
-// Make a request call to grab the HTML body from the site of your choice
-  request(websiteURL, function(error, response, html) {
-  
-    // Load the HTML into cheerio and save it to a variable
-    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-    var $ = cheerio.load(html);
-  
-    // An empty array to save the data that we'll scrape
-    // var results = [];
-  
-    // Select each element in the HTML body from which you want information.
-    // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-    // but be sure to visit the package's npm page to see how it works
-    $("div.news-list__block").each(function(i, element) {
-  
-      var title = $(element).find("h3.news-list__headline ").text();
-      var link = $(element).find("a").attr("href");
-  
-      // Save these results in an object that we'll push into the results array we defined earlier
-      db.articles.insert({
-        title: title,
-        link: "https://www.thestreet.com" + link
-
-      }, function (result){
-        res.json(result);
-      });
-    });
-  });
-}); 
-
-app.get("/all", function (req, res){
-  db.articles.find({}, function (error, found) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(found);
-     }
-  });
-});
-    // Log the results once you've looped through each of the elements found with cheerio
-    // console.log(results);
-
-
-//listening on port 3000
-app.listen(3000, function () {
-  console.log("App is running on port 3000 successfully!");
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
 });
 
+// Listen on port 3000
+app.listen(PORT, function() {
+  console.log("App running on PORT " + PORT);
+});
